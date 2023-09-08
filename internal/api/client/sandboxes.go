9bbd5f4991e57094472cdfc66a63a9029f353579
Jordan Acosta <jordanjacosta@gmail.com>
2023-09-07 23:37:47 -0700

    feat: short-term work-around to get SDK
    Since we haven't published the SDK yet, just copying it into this repo
    until the SDK repo is set up.

---
+++ internal/api/client/sandboxes.go
@@ -0,0 +1,45 @@
+package client
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/nuonco/terraform-provider-nuon/internal/api/client/client/operations"
+	"github.com/nuonco/terraform-provider-nuon/internal/api/client/models"
+)
+
+// sandbox methods
+func (c *client) GetSandboxes(ctx context.Context) ([]*models.AppSandbox, error) {
+	resp, err := c.genClient.Operations.GetV1Sandboxes(&operations.GetV1SandboxesParams{
+		Context: ctx,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("unable to get sandboxes: %w", err)
+	}
+
+	return resp.Payload, nil
+}
+
+func (c *client) GetSandbox(ctx context.Context, sandboxID string) (*models.AppSandbox, error) {
+	resp, err := c.genClient.Operations.GetV1SandboxesSandboxID(&operations.GetV1SandboxesSandboxIDParams{
+		SandboxID: sandboxID,
+		Context:   ctx,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("unable to get sandbox: %w", err)
+	}
+
+	return resp.Payload, nil
+}
+
+func (c *client) GetSandboxReleases(ctx context.Context, sandboxID string) ([]*models.AppSandboxRelease, error) {
+	resp, err := c.genClient.Operations.GetV1SandboxesSandboxIDReleases(&operations.GetV1SandboxesSandboxIDReleasesParams{
+		SandboxID: sandboxID,
+		Context:   ctx,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("unable to get sandbox releases: %w", err)
+	}
+
+	return resp.Payload, nil
+}
