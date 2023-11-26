6b70a74bfd37ceb829389cbadeb4b521a968d532
Jon Morehouse <morehousej09@gmail.com>
2023-11-26 17:19:57 -0600

    fix: properly handle state change errors

--- internal/provider/errs.go
+++ internal/provider/errs.go
@@ -2,10 +2,12 @@ package provider
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/nuonco/nuon-go"
 )
 
@@ -14,6 +16,11 @@ func writeDiagnosticsErr(_ context.Context, diagnostics *diag.Diagnostics, err e
 	userErr, ok := nuon.ToUserError(err)
 	if ok {
 		msg = fmt.Sprintf("Error: %s\n\nError Details: %s", userErr.Error, userErr.Description)
+	}
+
+	stateErr := &retry.UnexpectedStateError{}
+	if errors.As(err, &stateErr) {
+		msg = fmt.Sprintf("Error polling state change for resource. This may require manual intervention. %s\nError: %s", msg, err)
 	}
 
 	diagnostics.AddError(
